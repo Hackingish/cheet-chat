a4827b56e2366ff11002d6a417bbc43c0b4fe933
Hackingish <subhasispanda876@gmail.com>
2023-01-06 16:40:26 +0530

    final changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+import "./App.css";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="a960f356-6087-4845-b7e8-5b803d56a708"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
